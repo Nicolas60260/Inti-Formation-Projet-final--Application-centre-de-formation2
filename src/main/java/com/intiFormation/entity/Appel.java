f997c9fbb9478cb4b1fa5d5b9fb2af88a6250bb3
MaximeNj <enjolras.maxime83@gmail.com>
2023-09-18 13:38:05 +0200

    cmt entities Appel Commentaire

---
+++ src/main/java/com/intiFormation/entity/Appel.java
@@ -0,0 +1,180 @@
+package com.intiFormation.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+
+
+
+
+//Annotations à compléter
+@Entity
+public class Appel {
+	
+	
+	
+//////////////////////////Attributs inhérents à Commentaire//////////////////////////
+	int id;
+	Date debutAppel;
+	long duree;
+	boolean RDV;
+	
+//////////////////////////Attributs avec liens à d'autres classes//////////////////////////
+	
+	
+	@ManyToOne
+	@JoinColumn(name="idappel")
+	Commercial commercial;
+	
+	@OneToOne(mappedBy = "appel")
+	Commentaire commmentaire;
+	
+	//Relations à gérer
+	@ManyToOne
+	@JoinColumn(name="idappel")
+	Prospect prospect;
+
+	
+///////////////////////////Constructeurs////////////////////////// 
+
+
+//Constructeur tous paramètres
+	public Appel(int id, Date debutAppel, long duree, boolean rDV, Commercial commercial, Commentaire commmentaire,
+			Prospect prospect) {
+		super();
+		this.id = id;
+		this.debutAppel = debutAppel;
+		this.duree = duree;
+		RDV = rDV;
+		this.commercial = commercial;
+		this.commmentaire = commmentaire;
+		this.prospect = prospect;
+	}
+
+
+	
+	
+	// Constructeur par défaut
+	
+	public Appel() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+
+	
+///////////////////////////Getters setters////////////////////////// 
+
+	public int getId() {
+		return id;
+	}
+
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+
+	public Date getDebutAppel() {
+		return debutAppel;
+	}
+
+
+
+
+	public void setDebutAppel(Date debutAppel) {
+		this.debutAppel = debutAppel;
+	}
+
+
+
+
+	public long getDuree() {
+		return duree;
+	}
+
+
+
+
+	public void setDuree(long duree) {
+		this.duree = duree;
+	}
+
+
+
+
+	public boolean isRDV() {
+		return RDV;
+	}
+
+
+
+
+	public void setRDV(boolean rDV) {
+		RDV = rDV;
+	}
+
+
+
+
+	public Commercial getCommercial() {
+		return commercial;
+	}
+
+
+
+
+	public void setCommercial(Commercial commercial) {
+		this.commercial = commercial;
+	}
+
+
+
+
+	public Commentaire getCommmentaire() {
+		return commmentaire;
+	}
+
+
+
+
+	public void setCommmentaire(Commentaire commmentaire) {
+		this.commmentaire = commmentaire;
+	}
+
+
+
+
+	public Prospect getProspect() {
+		return prospect;
+	}
+
+
+
+
+	public void setProspect(Prospect prospect) {
+		this.prospect = prospect;
+	}
+
+
+///////////////////////////Méthode toString//////////////////////////
+
+	@Override
+	public String toString() {
+		return "Appel [id=" + id + ", debutAppel=" + debutAppel + ", duree=" + duree + ", RDV=" + RDV
+				+ ", commmentaire=" + commmentaire + "]";
+	}
+	
+	
+	
+}
