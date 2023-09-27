4d64786f06e549c049ccb4217d9494eb9437fe1a
MaximeNj <enjolras.maxime83@gmail.com>
2023-09-27 16:33:18 +0200

    16h33 27/09 avant pull nicolas

--- src/main/java/com/intiFormation/controller/UtilisateurController.java
+++ src/main/java/com/intiFormation/controller/UtilisateurController.java
@@ -37,6 +37,10 @@ public class UtilisateurController {
 	public Utilisateur getUtilisateur(@PathVariable("id") int id) {
 		return iutilisateurService.afficherParId(id);
 	}
+	@GetMapping(path = "/public/userByUsername/{username}")
+	public Utilisateur userByUsername(@PathVariable("username") String username){
+		return iutilisateurService.findByUsername(username).get();
+	}
 
 	@PostMapping(path = "/a/add") // ok
 	public void SaveUser(@RequestBody Utilisateur utilisateur) {
@@ -49,7 +53,7 @@ public class UtilisateurController {
 		 * conserver + de mémoire si la section est pas sollicitée.
 		 */
 		Date dateannee = new Date();
-		int annee = dateannee.getYear();
+		int annee = dateannee.getYear() + 1900;
 		String premiereLettre = utilisateur.getPrenom().substring(0, 1);
 		// Substring prénom
 		// getnom
